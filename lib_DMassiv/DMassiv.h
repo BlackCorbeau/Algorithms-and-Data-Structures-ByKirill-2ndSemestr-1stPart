f75e9382905c6004ddd3679f98e9d19352733c44
Remizov Kirirll <woron0987654321@gmail.com>
2024-09-09 19:59:16 +0300

    fixcommit! TPair have been change to tamplate

--- lib_DMassiv/DMassiv.h
+++ lib_DMassiv/DMassiv.h
@@ -19,8 +19,7 @@ namespace algorithms {
 
     template<typename T>
     T max(T val_1, T val_2) {
-        if (val_1 > val_2) { return val_1 }
-        else { return val_2 }
+        if (val_1 > val_2) { return val_1 } else { return val_2 }
     }
 }  // namespace algorithms
 

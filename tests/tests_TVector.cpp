78beb9c05d5d8cc999b18c43dccd54dd6012cf53
Kirill <kremizov3@gmail.com>
2024-10-14 21:32:31 +0300

    Switch tests

--- tests/tests_TVector.cpp
+++ tests/tests_TVector.cpp
@@ -44,17 +44,6 @@ TEST(TVectorTest, PushFront) {
     EXPECT_EQ(vec[2], 1);
 }
 
-TEST(TVectorTest, PopFront) {
-    TVector<int> vec;
-    vec.push_front(1);
-    vec.push_front(2);
-    vec.push_front(3);
-    vec.pop_front();
-    EXPECT_EQ(vec.size(), 2);
-    EXPECT_EQ(vec[0], 2);
-    EXPECT_EQ(vec[1], 1);
-}
-
 TEST(TVectorTest, Insert) {
     TVector<int> vec;
     vec.push_back(1);
@@ -66,55 +55,6 @@ TEST(TVectorTest, Insert) {
     EXPECT_EQ(vec[1], 4);
     EXPECT_EQ(vec[2], 2);
     EXPECT_EQ(vec[3], 3);
-}
-
-TEST(TVectorTest, Erase) {
-    TVector<int> vec;
-    vec.push_back(1);
-    vec.push_back(2);
-    vec.push_back(3);
-    vec.erase(1, 1);
-    EXPECT_EQ(vec.size(), 2);
-    EXPECT_EQ(vec[0], 1);
-    EXPECT_EQ(vec[1], 3);
-}
-
-TEST(TVectorTest, RemoveAll) {
-    TVector<int> vec;
-    vec.push_back(1);
-    vec.push_back(2);
-    vec.push_back(2);
-    vec.push_back(3);
-    vec.remove_all(2);
-    EXPECT_EQ(vec.size(), 2);
-    EXPECT_EQ(vec[0], 1);
-    EXPECT_EQ(vec[1], 3);
-}
-
-TEST(TVectorTest, RemoveFirst) {
-    TVector<int> vec;
-    vec.push_back(1);
-    vec.push_back(2);
-    vec.push_back(2);
-    vec.push_back(3);
-    vec.remove_first(2);
-    EXPECT_EQ(vec.size(), 3);
-    EXPECT_EQ(vec[0], 1);
-    EXPECT_EQ(vec[1], 2);
-    EXPECT_EQ(vec[2], 3);
-}
-
-TEST(TVectorTest, RemoveLast) {
-    TVector<int> vec;
-    vec.push_back(1);
-    vec.push_back(2);
-    vec.push_back(2);
-    vec.push_back(3);
-    vec.remove_last(2);
-    EXPECT_EQ(vec.size(), 3);
-    EXPECT_EQ(vec[0], 1);
-    EXPECT_EQ(vec[1], 2);
-    EXPECT_EQ(vec[2], 3);
 }
 
 TEST(TVectorTest, FindFirst) {

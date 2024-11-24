ebc8667de29f03b6f906afd25e37630ba13957c1
Kirill <kremizov3@gmail.com>
2024-11-24 20:52:59 +0300

    All work with iterators have been done, and also had some fixes with all last work

--- lib_Parser/Parser.cpp
+++ lib_Parser/Parser.cpp
@@ -6,26 +6,41 @@
 bool is_correct(const CString* exp) {
     Stack<char> st;
     const char* mass = exp->data();
+    char module_bracket = '\0';
+    
     for (size_t i = 0; i < exp->size() + 1; i ++) {
         if (mass[i] == '(' || mass[i] == '[' || mass[i] == '{') {
             st.push(mass[i]);
+        } else if (mass[i] == '|') {
+            if (module_bracket == '\0') {
+                // Встретили открывающую скобку модуля
+                module_bracket = '|';
+            } else {
+                // Встретили закрывающую скобку модуля
+                if (module_bracket == '|') {
+                    module_bracket = '\0'; // Сбрасываем переменную
+                } else {
+                    throw std::logic_error("Mismatched brackets found");
+                }
+            }
         } else if (mass[i] == ')' || mass[i] == ']' || mass[i] == '}') {
             if (st.empty()) {
-                st.push(mass[i]);
+                throw std::logic_error("Unmatched closing bracket found");
             } else {
+                char top = st.top();
                 st.pop();
+                if ((mass[i] == ')' && top != '(') ||
+                    (mass[i] == ']' && top != '[') ||
+                    (mass[i] == '}' && top != '{')) {
+                    throw std::logic_error("Mismatched brackets found");
+                }
             }
         }
     }
 
-    if (st.empty()) {
-        return true;
-    } else if (st.empty() == true &&
-    (st.top() == '(' || st.top() == '[' || st.top() == '{')) {
-        std::logic_error("Your string have unclosed '(' or '[' or '{'");
-        return false;
-    } else {
-        std::logic_error("Your string have unclosed ')' or ']' or '}'");
-        return false;
+    if (!st.empty() || module_bracket != '\0') {
+        throw std::logic_error("Unmatched opening bracket found");
     }
+
+    return true;
 }

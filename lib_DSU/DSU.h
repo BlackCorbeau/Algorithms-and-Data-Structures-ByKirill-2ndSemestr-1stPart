e60112ab5e5239daf8b58fbff63d8c27f649b50d
Kirill <kremizov3@gmail.com>
2025-02-10 17:38:14 +0300

    Added a based form of DSU

--- lib_DSU/DSU.h
+++ lib_DSU/DSU.h
@@ -2,18 +2,61 @@
 
 #ifndef LIB_DSU_LIB_DSU_HEDER_H_
 #define LIB_DSU_LIB_DSU_HEDER_H_
+#define SIZE_STEP 5 //просто затравочка на доработку
+
+#include "../lib_DMassiv/DMassiv.h"
 
 class DSU {
     int _size;
     int *_parent;
-    int *_rank;
- public:
+
+public:
     DSU(int size = 0);
     ~DSU();
+
     void make_set(int elem);
     int find(int elem);
-    void Union(int first, int second);
+    void union_sets(int first, int second);
     void clear();
 };
+
+DSU::DSU(int size){
+    _size = size;
+    _parent = new int[_size];
+    for (int i = 0; i < _size; ++i) {
+        make_set(i);
+    }
+}
+
+DSU::~DSU() {
+    delete[] _parent;
+    _parent = nullptr;
+    _size = 0;
+}
+
+void DSU::make_set(int elem) {
+    _parent[elem] = elem;
+}
+
+int DSU::find(int elem) {
+    while (elem != _parent[elem]) {
+        elem = _parent[elem];
+    }
+    return elem;
+}
+
+void DSU::union_sets(int first, int second) {
+    int root1 = find(first);
+    int root2 = find(second);
+
+    if (root1 != root2) {
+        _parent[root2] = root1;
+    }
+}
+
+void DSU::clear() {
+    delete[] _parent;
+    _size = 0;
+}
 
 #endif  // LIB_DMASSIVE_LIB_DMASSIVE_HEDER_H_

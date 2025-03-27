a9d7429d4c9c6ae41344cfa8c6fa6d3ec7644364
Kirill <kremizov3@gmail.com>
2025-03-27 21:48:16 +0300

    BinTree Table
    Initialized a bin tree table class

---
+++ lib_BinTreeTable/BinTreeTable.h
@@ -0,0 +1,31 @@
+// Copyright 2024 Kirill Remizov
+
+#ifndef LIB_BINTREETABLE_LIB_BINTREETABLE_HEDER_H_
+#define LIB_BINTREETABLE_LIB_BINTREETABLE_HEDER_H_
+
+#pragma once
+#include "../lib_BinTree/BinTree.h"
+#include "../lib_pair/lib_pair_heder.h"
+#include "../lib_Table/lib_table.h"
+
+template <class Tkey, class Tval>
+class BTTable : ITable<Tkey, Tval> {
+private:
+    BinTree<TPair<Tkey, Tval>> table;
+    size_t _size;
+
+public:
+    BTTable(): _size(0) {}
+
+    int size() {return _size;}
+
+    Tkey insert(Tval val) override;
+
+    void insert(Tkey key, Tval val) override;
+
+    void erase(Tkey key) override;
+
+    Tval find(Tkey key) override;
+
+    void print() const noexcept;
+};

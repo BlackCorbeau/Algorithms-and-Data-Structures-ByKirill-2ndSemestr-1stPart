435b0177c82b20d5b0fe34fe5aaacb6e04177aa5
Kirill <kremizov3@gmail.com>
2025-04-13 19:21:56 +0300

    SubTask for heap
    Subtask for Heap to sorting massiv by heap is clear and can be shown

--- main/main.cpp
+++ main/main.cpp
@@ -5,9 +5,32 @@
 
 #include <iostream>
 #include <chrono>
-#include "../lib_BinTree/BinTree.h"
+#include "../lib_Heap/Heap.h"
+
+template <class T>
+void heapSort(T* array, size_t size, heapType type) {
+
+    Heap<T> heap(array, size, type);
+
+    for (int i = size - 1; i >= 0; i--) {
+        array[i] = heap.top();
+        heap.eraise(heap.top());
+    }
+}
 
 int main() {
+    int arr[] = {12, 11, 13, 5, 6, 7};
+    int n = sizeof(arr)/sizeof(arr[0]);
+    heapSort(arr, n, MAX);
+    for (int i = 0; i < n; i++) {
+        std::cout << arr[i] << " ";
+    }
+    std::cout << "\n";
+    heapSort(arr, n, MIN);
+    for (int i = 0; i < n; i++) {
+        std::cout << arr[i] << " ";
+    }
+return 0;
 }
 
 #endif  // EASY_EXAMPLE

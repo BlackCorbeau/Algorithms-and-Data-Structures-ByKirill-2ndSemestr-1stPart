f9e3e4e2bb4d466e77a7efa0e5b98b7d5d0fa11a
Kirill <kremizov3@gmail.com>
2025-04-20 21:35:50 +0300

    feat(ListHashTable): structure ListNode
    Initialized class ListNode with will be a main usage class in List Hash Table.
    Added:
      Based constructor;
      Initialized constructor;
      Setters and getters for list with values;
      Setters and getters for state;

---
+++ lib_HashTable/ListHashTable.h
@@ -0,0 +1,40 @@
+// Copyright 2024 Kirill Remizov
+
+#ifndef LIB_HEAP_LIB_HASHTABLE_HEDER_H_
+#define LIB_HEAP_LIB_HASHTABLE_HEDER_H_
+
+#include <cstddef>
+#include <functional>
+#include <icecream.hpp>
+#include "../lib_pair/lib_pair_heder.h"
+#include "../lib_list/List.h"
+#include <stdexcept>
+#define CAPACITY 15
+
+enum States {EMPTY, DELETED, BUISY};
+
+template <class Tkey, class Tval>
+struct HashList {
+private:
+    TList<TPair<Tkey, Tval>> _val;
+    States _state;
+public:
+    HashList() : _val(), _state(EMPTY) {}
+    HashList(Tkey key, Tval val) : _val(key, val), _state(BUISY) {}
+
+    TList<TPair<Tkey, Tval>>* get_val(){
+        return *_val;
+    }
+    void set_val(TList<TPair<Tkey, Tval>>* val){
+        _val = &val;
+    }
+
+    States get_state(){
+        return _state;
+    }
+    void set_state(States state){
+        _state = state;
+    }
+};
+
+#endif  // LIB_HEAP_LIB_HASHTABLE_HEDER_H_
